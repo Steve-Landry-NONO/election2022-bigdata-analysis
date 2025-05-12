5fc7639173ed26e0af68fbcaf9240ce92b8650f8
steve-landry.kouokam-nono <steve-landry.kouokam-nono@ens.uvsq.fr>
2025-05-12 17:13:30 +0200

    Statistiques descriptive: pour mieux comprendre la structure du jeu

--- R/exploration.R
+++ R/exploration.R
@@ -7,3 +7,12 @@ library(janitor)
 # Chargement des données fusionnées
 data <- read_csv("data/base_fusion.csv") %>%
   clean_names()
+
+# Aperçu global
+glimpse(data)
+
+# Résumé statistique de toutes les colonnes numériques
+summary(data)
+
+# Nombre de valeurs manquantes par colonne
+colSums(is.na(data))
